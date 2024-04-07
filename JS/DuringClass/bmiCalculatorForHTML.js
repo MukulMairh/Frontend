a5d699b087aabc7ab443e7283be2ed16aabdbb03
Mukul <msoni7718@gmail.com>
2024-04-08 00:40:33 +0530

    An attempt to create the bmi calculator using the html, css, js that will run eventually on the browser.

--- JS/DuringClass/bmiCalculatorForHTML.js
+++ JS/DuringClass/bmiCalculatorForHTML.js
@@ -1,56 +1,44 @@
-
-//Masses in kgs
-const jhonMass = 60;
-const markMass = 55;
-
-//Height's in meters 
-const jhonHeight = 1.65;
-const markHeight = 1.60;
+let jhonHeight = 1, jhonWeight = 1, markHeight = 1, markWeight = 1;
 
 
 
+//bmi calculation logic
 function bmiCalculator(mass, height) {
     return mass / (height * height)
 }
 
-let jhonBMI = bmiCalculator(jhonMass, jhonHeight);
-let markBMI = bmiCalculator(markMass, markHeight);
+let jhonBMI; let markBMI;
+// getBMIs is a anonymous function.
+function getBMIs() {
+    jhonBMI = bmiCalculator(jhonWeight, jhonHeight);
+    markBMI = bmiCalculator(markWeight, markHeight);
+}
 
+//submitValues function will get the values submitted from frontend
+function submitValues() {
+    //fetching the values from html 
+    let htmlJhonHeight = document.getElementById("jhonHeight").value;
+    jhonHeight = htmlJhonHeight;
+    let htmlJhonWeight = document.getElementById("jhonWeight").value;
+    jhonWeight = htmlJhonWeight;
+    //Mark's 
+    let htmlMarkHeight = document.getElementById("markHeight").value;
+    markHeight = htmlMarkHeight;
+    let htmlMarkWeight = document.getElementById("markWeight").value;
+    markWeight = htmlMarkWeight;
 
-function bmiCalculation() {
-    setTimeout(() => {
-        let htmlJhonHeight = document.getElementById("jhonHeight").innerHTML;
-        htmlJhonHeight = htmlJhonHeight + ` = ${jhonMass} in kgs`;
-    }, 2000);
+    //calling the calculation process
+    getBMIs();
 
 
 }
 
-bmiCalculation();
+//Creating element to show the element to show the bmi values *2
 
 
+//Creting an element to show bmi comparision
 
 
-
-
-
-
-
-
-
-
-
-
-//String contatenation
-console.log("Height of Jhon " + jhonHeight + " meters");
-console.log("Weight of Jhon " + jhonMass + " kgs");
-
-console.log(`Height of Mark is ${markHeight} meters `);
-console.log(`Height of Mark is ${markMass} kgs`);
-
-console.log("JhonBMI Round offs = " + jhonBMI.toFixed(2));
-console.log("markBMI Round offs = " + markBMI.toFixed(2));
-
-//ternary operators and string literals 
-jhonBMI > markBMI ? console.log(`Jhon's BMI(${jhonBMI}) is greater than Mark's BMI(${markBMI}).`) :
-    console.log(`Mark's BMI(${markBMI}) is greater than Jhon's BMI(${jhonBMI}).`); 
+//ternary operators and string literals
+// jhonBMI > markBMI ? console.log(`Jhon's BMI(${jhonBMI}) is greater than Mark's BMI(${markBMI}).`) :
+//     console.log(`Mark's BMI(${markBMI}) is greater than Jhon's BMI(${jhonBMI}).`); 
